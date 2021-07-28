9bacabb760b3d1cac0619517603833b0eb9f1a11
IlarioCOCCO <ilario.cocco@gmail.com>
2021-07-28 15:45:18 +0200

    test commit

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+
+var nome = prompt("digita il tuo nome");
+alert (nome)
+
+/* 2. chiedo all'utente il suo cognome */
+var cognome = ("ilario");
+
+/* 3. chiedo all'utente il suo colore preferito  */
+var colorepreferito = ("ilario");
+
+/* 4. scrivo sulla pagina nomecognomecolorepreferito40 */
