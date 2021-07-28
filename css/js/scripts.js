977692e7c97915a5151f69bd8263f4af3bd81e20
IlarioCOCCO <ilario.cocco@gmail.com>
2021-07-28 16:04:12 +0200

    commit

--- css/js/scripts.js
+++ css/js/scripts.js
@@ -1,15 +1,15 @@
     var nome = prompt("digita il tuo nome");
-    document.getElementById("nome").innerHTML = nome;
+    document.getElementById("nome").innerHTML += nome;
 
 
     /* 2. chiedo all'utente il suo cognome */
-    var cognome = ("dgita il tuo cognome");
-    document.getElementById("cognome").innerHTML = cognome;
+    var cognome = prompt("dgita il tuo cognome");
+    document.getElementById("cognome").innerHTML += cognome;
 
 
     /* 3. chiedo all'utente il suo colore preferito  */
-    var colorepreferito = ("colorepreferito");
-    document.getElementById("cognome").innerHTML = colorepreferito;
+    var colorepreferito = prompt("colorepreferito");
+    document.getElementById("colorepreferito").innerHTML += colorepreferito;
 
     /* 4. scrivo sulla pagina nomecognomecolorepreferito40 */
 
